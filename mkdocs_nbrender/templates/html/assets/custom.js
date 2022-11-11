75d972c59834a93c067cd3ef60f33361a5257f9b
onceanumeric <numerical.ocean@gmail.com>
2022-11-11 23:18:45 +0100

    [update] style and js

--- mkdocs_nbrender/templates/html/assets/custom.js
+++ mkdocs_nbrender/templates/html/assets/custom.js
@@ -2,3 +2,11 @@
 body_dom = document.getElementsByTagName("BODY")[0]
 // add class name
 body_dom.classList.add("jupyter-body")
+
+// change svg width
+var div_svg = document.getElementsByClassName('jp-RenderedSVG');
+
+for (const element of div_svg) {
+    element.childNodes[3].setAttribute("width", "100%")
+    element.childNodes[3].setAttribute("height", "auto")
+}
