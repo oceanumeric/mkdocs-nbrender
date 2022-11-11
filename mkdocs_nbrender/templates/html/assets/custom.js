6ab80d1a11c3b848121fa5a8551d7bbb942ecd58
onceanumeric <numerical.ocean@gmail.com>
2022-11-11 23:35:52 +0100

    [update] overflow style

--- mkdocs_nbrender/templates/html/assets/custom.js
+++ mkdocs_nbrender/templates/html/assets/custom.js
@@ -1,12 +1,7 @@
-// clean body attribute 
-body_dom = document.getElementsByTagName("BODY")[0]
-// add class name
-body_dom.classList.add("jupyter-body")
 
 // change svg width
 var div_svg = document.getElementsByClassName('jp-RenderedSVG');
-
 for (const element of div_svg) {
     element.childNodes[3].setAttribute("width", "100%")
-    element.childNodes[3].setAttribute("height", "auto")
+    element.childNodes[3].setAttribute("height", "100%")
 }
