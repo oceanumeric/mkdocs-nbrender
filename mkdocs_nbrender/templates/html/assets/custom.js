533ac26660df46dd3dad8b86e68297a5ab3a1612
onceanumeric <numerical.ocean@gmail.com>
2022-11-13 00:17:33 +0100

    [update] the style

--- mkdocs_nbrender/templates/html/assets/custom.js
+++ mkdocs_nbrender/templates/html/assets/custom.js
@@ -7,6 +7,29 @@ for (const element of div_svg) {
 }
 
 // create a toc div
-var toc_div = document.createElement("div")
-toc_div.classList.add("toc")
-document.body.insertBefore(toc_div, document.body.firstChild);
+var toc_div = document.createElement("nav")
+toc_div.classList.add("nav-toc")
+document.body.append(toc_div)
+
+// add toc 
+nav_toc = ""
+all_h2 = document.getElementsByTagName('h2'); 
+var new_line, h2_txt, h2_a; 
+
+for (const element of all_h2) {
+    h2_txt = element.textContent; 
+    h2_a = "#" + element.attributes.id.textContent
+    // slice the last tag
+    h2_txt = h2_txt.slice(0, h2_txt.length-1)
+    
+    new_line =
+    "<li>" + "<a href='" + h2_a + "'>" + h2_txt  + "</a>" + "</li>";
+
+    nav_toc += new_line; 
+}
+
+
+// select nav-toc 
+
+dom_nav_toc = document.getElementsByClassName("nav-toc");
+dom_nav_toc[0].innerHTML += nav_toc; 
